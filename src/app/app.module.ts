584546a14b2ee01d8170e21ab10b6256a9059f7b
Stan <kopacb007@gmail.com>
2017-07-17 23:23:49 +0200

    routing mess

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from "@angular/router";
+import { AppRoutingModule } from "./app-routing.module";
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from "./heroes.component";
+import { DashboardComponent } from './dashboard.component';
+import { HeroDetailComponent } from "./hero-detail.component";
+
+import { HeroService } from "./hero.service";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    DashboardComponent,
+    HeroDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
